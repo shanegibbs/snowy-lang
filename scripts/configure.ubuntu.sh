5f46c57404add43738fd2ee2946424f0c7fef19d
Shane Gibbs <shane@hands.net.nz>
2014-12-05 23:48:52 -0600

    Run ubuntu configure script from anywhere

--- scripts/configure.ubuntu.sh
+++ scripts/configure.ubuntu.sh
@@ -3,4 +3,4 @@
 echo sudo apt-get install build-essential autoconf clang-3.5 llvm-3.5 llvm-3.5-dev libedit-dev libglib2.0-0 libglib2.0-dev \
   bison
 
-../configure CXX=clang++ CXXFLAGS='-g -std=c++11 -Wno-deprecated-register' --enable-debug
+`dirname "$(readlink -f $0)"`/../configure CXX=clang++ CXXFLAGS='-g -std=c++11 -Wno-deprecated-register' --enable-debug
