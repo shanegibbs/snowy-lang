1934e7c809aab9e940c1f16327a86b0abbfb7435
Shane Gibbs <shane@hands.net.nz>
2014-12-20 21:15:27 -0600

    Add travis/script.sh

--- scripts/travis/script.sh
+++ scripts/travis/script.sh
@@ -3,9 +3,9 @@
 curl -O 'https://s3-us-west-2.amazonaws.com/snowy-lang/deps/travis/snowy-deps.tar.gz'
 tar -xzf snowy-deps.tar.gz -C $HOME
 
-export PATH=/home/travis/snowy-deps/glib-2.43.2/bin:/home/travis/snowy-deps/bison-3.0.2/bin:/home/travis/snowy-deps/flex-2.5.39/bin:/home/travis/snowy-deps/llvm-3.5.0/bin:/home/sgibbs/snowy-deps/gcc-4.8.4/bin:/usr/bin:/bin
-export LD_SEARCH_PATH=/home/travis/snowy-deps/glib-2.43.2/lib:/home/travis/snowy-deps/glib-2.43.2/bin:/home/travis/snowy-deps/bison-3.0.2/bin:/home/sgibbs/snowy-deps/flex-2.5.39/bin:/home/sgibbs/snowy-deps/llvm-3.5.0/bin:/home/sgibbs/snowy-deps/gcc-4.8.4/bin:/usr/bin:/bin
-export PKG_CONFIG_PATH=/home/travis/snowy-deps/glib-2.43.2/lib/pkgconfig:/home/travis/snowy-deps/glib-2.43.2/bin:/home/travis/snowy-deps/bison-3.0.2/bin:/home/sgibbs/snowy-deps/flex-2.5.39/bin:/home/sgibbs/snowy-deps/llvm-3.5.0/bin:/home/sgibbs/snowy-deps/gcc-4.8.4/bin:/usr/bin:/bin
+export PATH=/home/travis/snowy-deps/glib-2.43.2/bin:/home/travis/snowy-deps/bison-3.0.2/bin:/home/travis/snowy-deps/flex-2.5.39/bin:/home/travis/snowy-deps/llvm-3.5.0/bin:/home/travis/snowy-deps/gcc-4.8.4/bin:/usr/bin:/bin
+export LD_SEARCH_PATH=/home/travis/snowy-deps/glib-2.43.2/lib:/home/travis/snowy-deps/glib-2.43.2/bin:/home/travis/snowy-deps/bison-3.0.2/bin:/home/travis/snowy-deps/flex-2.5.39/bin:/home/travis/snowy-deps/llvm-3.5.0/bin:/home/travis/snowy-deps/gcc-4.8.4/bin:/usr/bin:/bin
+export PKG_CONFIG_PATH=/home/travis/snowy-deps/glib-2.43.2/lib/pkgconfig:/home/travis/snowy-deps/glib-2.43.2/bin:/home/travis/snowy-deps/bison-3.0.2/bin:/home/travis/snowy-deps/flex-2.5.39/bin:/home/travis/snowy-deps/llvm-3.5.0/bin:/home/travis/snowy-deps/gcc-4.8.4/bin:/usr/bin:/bin
 
 export CXXFLAGS=''
 
