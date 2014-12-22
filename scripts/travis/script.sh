f5eb084d5f46f0e409992c4053f4e987592175c8
Shane Gibbs <shane@hands.net.nz>
2014-12-22 13:30:56 -0600

    Display debug info

--- scripts/travis/script.sh
+++ scripts/travis/script.sh
@@ -1,5 +1,15 @@
 #!/bin/bash -ex
 
+echo Initial libraries
+ldconfig -p |sort
+
+export PATH=/home/travis/snowy-deps/glib-2.43.2/bin:/home/travis/snowy-deps/bison-3.0.2/bin:/home/travis/snowy-deps/flex-2.5.39/bin:/home/travis/snowy-deps/llvm-3.5.0/bin:/home/travis/snowy-deps/gcc-4.8.4/bin:/usr/sbin:/sbin:/usr/bin:/bin
+export LD_SEARCH_PATH=/home/travis/snowy-deps/libedit-3.1/lib:/home/travis/snowy-deps/glib-2.43.2/lib:/home/travis/snowy-deps/glib-2.43.2/bin:/home/travis/snowy-deps/bison-3.0.2/bin:/home/travis/snowy-deps/flex-2.5.39/bin:/home/travis/snowy-deps/llvm-3.5.0/bin:/home/travis/snowy-deps/gcc-4.8.4/bin:/usr/bin:/bin
+export PKG_CONFIG_PATH=/home/travis/snowy-deps/libedit-3.1/lib/pkgconfig:/home/travis/snowy-deps/glib-2.43.2/lib/pkgconfig:/home/travis/snowy-deps/glib-2.43.2/bin:/home/travis/snowy-deps/bison-3.0.2/bin:/home/travis/snowy-deps/flex-2.5.39/bin:/home/travis/snowy-deps/llvm-3.5.0/bin:/home/travis/snowy-deps/gcc-4.8.4/bin:/usr/bin:/bin
+
+echo Installed libraries
+ldconfig -p |sort
+
 export CXXFLAGS=''
 
 mkdir m4 && ./autogen.sh
