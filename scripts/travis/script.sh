78e24691ffdd6cd62f2ff762c4273b5918534492
Shane Gibbs <shane@hands.net.nz>
2014-12-22 23:39:13 -0600

    Update LD_SEARCH_PATH

--- scripts/travis/script.sh
+++ scripts/travis/script.sh
@@ -4,7 +4,7 @@ echo Initial libraries
 ldconfig -p |sort
 
 export PATH=/home/travis/snowy-deps/glib-2.43.2/bin:/home/travis/snowy-deps/bison-3.0.2/bin:/home/travis/snowy-deps/flex-2.5.39/bin:/home/travis/snowy-deps/llvm-3.5.0/bin:/home/travis/snowy-deps/gcc-4.8.4/bin:/usr/sbin:/sbin:/usr/bin:/bin
-export LD_SEARCH_PATH=/home/travis/snowy-deps/libedit-3.1/lib:/home/travis/snowy-deps/glib-2.43.2/lib:/home/travis/snowy-deps/glib-2.43.2/bin:/home/travis/snowy-deps/bison-3.0.2/bin:/home/travis/snowy-deps/flex-2.5.39/bin:/home/travis/snowy-deps/llvm-3.5.0/bin:/home/travis/snowy-deps/gcc-4.8.4/bin:/usr/bin:/bin
+export LD_SEARCH_PATH=/home/travis/snowy-deps/libedit-3.1/lib:/home/travis/snowy-deps/glib-2.43.2/lib:/home/travis/snowy-deps/glib-2.43.2/lib:/home/travis/snowy-deps/bison-3.0.2/lib:/home/travis/snowy-deps/flex-2.5.39/lib:/home/travis/snowy-deps/llvm-3.5.0/lib:/home/travis/snowy-deps/gcc-4.8.4/lib:/home/travis/snowy-deps/gcc-4.8.4/lib64
 export PKG_CONFIG_PATH=/home/travis/snowy-deps/libedit-3.1/lib/pkgconfig:/home/travis/snowy-deps/glib-2.43.2/lib/pkgconfig:/home/travis/snowy-deps/glib-2.43.2/bin:/home/travis/snowy-deps/bison-3.0.2/bin:/home/travis/snowy-deps/flex-2.5.39/bin:/home/travis/snowy-deps/llvm-3.5.0/bin:/home/travis/snowy-deps/gcc-4.8.4/bin:/usr/bin:/bin
 
 echo Installed libraries
