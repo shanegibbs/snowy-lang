986106ef9be540786ea40621813c9401cd0ca240
Shane Gibbs <shane@hands.net.nz>
2014-12-23 23:21:42 -0600

    Add travis envs

--- scripts/travis/script.sh
+++ scripts/travis/script.sh
@@ -1,15 +1,9 @@
 #!/bin/bash -ex
 
-#echo Initial libraries
-#ldconfig -p |sort
-
 export PATH=/home/travis/snowy-deps/bin:/usr/sbin:/sbin:/usr/bin:/bin
 export LD_SEARCH_PATH=/home/travis/snowy-deps/lib:/home/travis/snowy-deps/lib64
 export PKG_CONFIG_PATH=/home/travis/snowy-deps/lib/pkgconfig
 
-#echo Installed libraries
-#ldconfig -p |sort
-
-mkdir m4 && ./autogen.sh
+mkdir -p m4 && ./autogen.sh
 
 ./configure CXXFLAGS='-Wl,--rpath=/home/travis/snowy-deps/lib64 -Wl,--rpath=/home/travis/snowy-deps/lib -I/home/travis/snowy-deps/include -L/home/travis/snowy-deps/lib' && make -j && make check
