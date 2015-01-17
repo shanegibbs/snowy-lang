11aad3cf6b897ba10e61184869bb602fee1fe9a4
Shane Gibbs <shane@hands.net.nz>
2015-01-17 10:58:02 -0600

    Allow tests to be filtered. Run tests in serial mode to see stdout.

--- tests/SnowyTestSuite.h
+++ tests/SnowyTestSuite.h
@@ -19,10 +19,16 @@ typedef void (*AddTestsFunc)(TestSuite&);
 class Test
 {
 public:
-    Test(const string path, TestFunc f) : path(path), f(f) {
+    Test(const string path, TestFunc f) : path(path), f(f)
+    {
         Log::setup();
     }
-    void run() const {
+    int run(string* filter) const
+    {
+        if (filter != nullptr && path.compare(0, filter->size(), *filter) != 0) {
+            return 0;
+        }
+
         cerr << path << ": ";
         try {
             f();
@@ -30,11 +36,14 @@ public:
         } catch (FatalLogEvent e) {
             cerr << "FAIL" << endl;
         }
+        return 1;
     }
-    static bool SortAsc(const Test* a, const Test* b) {
+    static bool SortAsc(const Test* a, const Test* b)
+    {
         return a->path.compare(b->path) < 0;
     }
-    static bool SortDesc(const Test* a, const Test* b) {
+    static bool SortDesc(const Test* a, const Test* b)
+    {
         return a->path.compare(b->path) > 0;
     }
 private:
@@ -45,58 +54,84 @@ private:
 class TestSuite
 {
 public:
-    TestSuite() {}
-    ~TestSuite() {
+    TestSuite()
+    {
+        single_pass = false;
+        filter = nullptr;
+        tests_run_count = 0;
+    }
+    ~TestSuite()
+    {
         for (const Test* t : script) {
             delete t;
         }
+        if (filter != nullptr) {
+            delete filter;
+        }
     }
-    void add(const string path, TestFunc f) {
+    void setFilter(string* f)
+    {
+        filter = f;
+        single_pass = true;
+    }
+    void add(const string path, TestFunc f)
+    {
         script.push_back(new Test(path, f));
     }
-    void add(const AddTestsFunc f) {
+    void add(const AddTestsFunc f)
+    {
         f(*this);
     }
-    int run() {
+    int run()
+    {
         int r;
         auto start_time = chrono::high_resolution_clock::now();
 
-        cerr << "* Running tests in natural order..." << endl;
+        if (!single_pass) {
+            cerr << "* Running tests in natural order..." << endl;
+        }
+
         r = run_script();
         if (r != 0) {
             return r;
         }
 
-        cerr << "* Running tests in ascending order..." << endl;
-        sort(script.begin(), script.end(), Test::SortAsc);
-        r = run_script();
-        if (r != 0) {
-            return r;
-        }
+        if (!single_pass) {
+            cerr << "* Running tests in ascending order..." << endl;
+            sort(script.begin(), script.end(), Test::SortAsc);
+            r = run_script();
+            if (r != 0) {
+                return r;
+            }
 
-        cerr << "* Running tests in descending order..." << endl;
-        sort(script.begin(), script.end(), Test::SortDesc);
-        r = run_script();
-        if (r != 0) {
-            return r;
+            cerr << "* Running tests in descending order..." << endl;
+            sort(script.begin(), script.end(), Test::SortDesc);
+            r = run_script();
+            if (r != 0) {
+                return r;
+            }
         }
 
         auto time = chrono::high_resolution_clock::now() - start_time;
         cerr << "----------------------------------------" << endl;
         double seconds = chrono::duration_cast<chrono::milliseconds>(time).count() / 1000.0f;
-        cerr << "Ran " << script.size() * 3 << " tests in " << seconds << "s" << endl;
+        cerr << "Ran " << tests_run_count << " tests in " << seconds << "s" << endl;
 
         cerr << endl << "\x1b[32mOK\x1b[0m" << endl;
         return 0;
     }
-    int run_script() {
+    int run_script()
+    {
         for (const Test* t : script) {
-            t->run();
+            tests_run_count += t->run(filter);
         }
         return 0;
     }
 private:
     vector<const Test*> script;
+    int tests_run_count;
+    bool single_pass;
+    string* filter;
 };
 
 }
