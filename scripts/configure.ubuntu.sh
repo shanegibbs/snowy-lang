1cb82be72255aa1e7969a3933ab3a319004d379c
Shane Gibbs <shane@hands.net.nz>
2015-06-15 22:09:54 -0500

    Fix configure script

--- scripts/configure.ubuntu.sh
+++ scripts/configure.ubuntu.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+CLANG=$(which clang++)
+if [ -z $CLANG ]; then
+    CLANG=$(which clang++-3.5)
+fi
+if [ -z $CLANG ]; then
+    echo clang++ and clang++-3.5 not found
+    exit 1
+fi
+
 `dirname "$(readlink -f $0)"`/../configure \
-  CXX='clang++' \
+  CXX=$CLANG \
   CXXFLAGS='-g -std=c++11 -Wno-deprecated-register'\
   --enable-debug
